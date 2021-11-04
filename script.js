ff2d8979a6fe43f297fb99f089a4cdacf1be5fda
denchik <kivadan@inbox.ru>
2021-11-04 03:06:04 +0300

    gameplay changes

--- script.js
+++ script.js
@@ -4,10 +4,14 @@ let CONTEXT=null;
 let SCALER=0.8;
 let SIZE={x:0,y:0,width:0,height:0,rows:3,columns:3};
 let PIECES=[];
+let SELECTED_PIECE=null;
+let START_TIME=null;
+let END_TIME=null;
 
 function main(){
     CANVAS=document.getElementById("myCanvas");
     CONTEXT=CANVAS.getContext("2d");
+    addEventListeners()
 
     let promise = navigator.mediaDevices.getUserMedia({video:true});
     promise.then(function (signal){
@@ -18,11 +22,97 @@ function main(){
         VIDEO.onloadeddata=function (){
             handleResize();
             //window.addEventListener('resize',handleResize);
+            initializePieces(SIZE.rows,SIZE.columns);
             updateCanvas();
         }
     }).catch(function (err){
         alert("Camera error"+err);
     })
+}
+
+function setDifficulty(){
+    let diff = document.getElementById("difficulty").value;
+    switch (diff){
+        case "easy":
+            initializePieces(3,3);
+            break;
+        case "medium":
+            initializePieces(10,10);
+            break;
+        case "hard":
+            initializePieces(20,20);
+            break;
+        case "insane":
+            initializePieces(40,25);
+            break;
+    }
+}
+
+function restart(){
+    START_TIME=new Date().getTime();
+    END_TIME=null;
+    randomizePieces()
+}
+
+function addEventListeners(){
+    CANVAS.addEventListener("mousedown",onMouseDown);
+    CANVAS.addEventListener("mousemove",onMouseMove);
+    CANVAS.addEventListener("mouseup",onMouseUp);
+    CANVAS.addEventListener("touchstart",onTouchStart);
+    CANVAS.addEventListener("touchmove",onTouchMove);
+    CANVAS.addEventListener("touchend",onTouchEnd);
+}
+
+function onTouchStart(evt){
+    let loc={x:evt.touches[0].clientX,
+        y:evt.touches[0].clientY};
+    onMouseDown(loc);
+}
+function onTouchMove(evt){
+    let loc={x:evt.touches[0].clientX,
+        y:evt.touches[0].clientY};
+    onMouseMove(loc);
+}
+function onTouchEnd(){
+    onMouseUp();
+}
+
+function onMouseDown(evt){
+    SELECTED_PIECE=getPressedPiece(evt);
+    if (SELECTED_PIECE!=null){
+        const index=PIECES.indexOf(SELECTED_PIECE);
+        if (index>-1){
+            PIECES.splice(index,1);
+            PIECES.push(SELECTED_PIECE);
+        }
+        SELECTED_PIECE.offset={
+            x:evt.x-SELECTED_PIECE.x,
+            y:evt.y-SELECTED_PIECE.y
+        }
+    }
+}
+
+function onMouseMove(evt){
+    if(SELECTED_PIECE!=null){
+        SELECTED_PIECE.x=evt.x-SELECTED_PIECE.offset.x;
+        SELECTED_PIECE.y=evt.y-SELECTED_PIECE.offset.y;
+    }
+}
+
+function onMouseUp(){
+    if (SELECTED_PIECE.isClose()){
+        SELECTED_PIECE.snap();
+    }
+    SELECTED_PIECE=null;
+}
+
+function getPressedPiece(loc){
+    for(let i=PIECES.length-1;i>=0;i--){
+        if (loc.x>PIECES[i].x && loc.x<PIECES[i].x+PIECES[i].width && loc.y>PIECES[i].y && loc.y<PIECES[i].y+PIECES[i].height){
+            return PIECES[i];
+        }
+    }
+    return null;
 }
 
 function handleResize(){
@@ -41,21 +131,38 @@ function handleResize(){
 }
 
 function updateCanvas(){
+    CONTEXT.clearRect(0,0,CANVAS.width,CANVAS.height);
+
+    CONTEXT.globalAlpha=0.5;
     CONTEXT.drawImage(VIDEO,
         SIZE.x, SIZE.y,
         SIZE.width, SIZE.height);
+    CONTEXT.globalAlpha=1;
     for (let i=0;i<PIECES.length;i++){
         PIECES[i].draw(CONTEXT)
     }
     window.requestAnimationFrame(updateCanvas);
 }
 
-function initializePieces(){
+function initializePieces(rows,cols){
+    SIZE.rows=rows;
+    SIZE.columns=cols;
     PIECES=[];
     for(let i=0;i<SIZE.rows;i++){
         for (let j=0;j<SIZE.columns;j++){
             PIECES.push(new Piece(i,j));
         }
+    }
+}
+
+function randomizePieces(){
+    for(let i=0;i<PIECES.length;i++){
+        let loc={
+            x:Math.random()*(CANVAS.width-PIECES[i].width),
+            y:Math.random()*(CANVAS.height-PIECES[i].height)
+        }
+        PIECES[i].x=loc.x;
+        PIECES[i].y=loc.y;
     }
 }
 
@@ -67,10 +174,41 @@ class Piece{
         this.height=SIZE.height/SIZE.rows;
         this.x=SIZE.x+SIZE.width*this.colIndex/SIZE.columns;
         this.y=SIZE.y+SIZE.height*this.rowIndex/SIZE.rows;
+        this.xCorrect=this.x;
+        this.yCorrect=this.y;
     }
     draw(context){
         context.beginPath();
+
+        context.drawImage(VIDEO,
+            this.colIndex*VIDEO.videoWidth/SIZE.columns,
+            this.rowIndex*VIDEO.videoHeight/SIZE.rows,
+            VIDEO.videoWidth/SIZE.columns,
+            VIDEO.videoHeight/SIZE.rows,
+            this.x,
+            this.y,
+            this.width,
+            this.height);
+
         context.rect(this.x,this.y,this.width,this.height);
         context.stroke();
     }
+    isClose(){
+        if (distance({x:this.x,y:this.y},
+            {x:this.xCorrect,y:this.yCorrect})<this.width/3){
+            return true;
+        }
+        return false;
+    }
+    snap(){
+        this.x=this.xCorrect;
+        this.y=this.yCorrect;
+    }
+}
+
+function distance(p1,p2){
+    return Math.sqrt(
+        (p1.x-p2.x)*(p1.x-p2.x)+
+        (p1.y-p2.y)*(p1.y-p2.y)
+    )
 }
